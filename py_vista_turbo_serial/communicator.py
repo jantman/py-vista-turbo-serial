b99a51559f53471056f2461befa2c4deb3be49d9
Jason Antman <jason@jasonantman.com>
2023-10-22 19:26:34 -0400

    WIP with real panel

--- py_vista_turbo_serial/communicator.py
+++ py_vista_turbo_serial/communicator.py
@@ -74,7 +74,7 @@ class Communicator:
         if self.outgoing:
             msg = self.outgoing.pop(0)
             logger.info('Sending message: %s', msg)
-            self.serial.write(msg + '\r\n')
+            self.serial.write((msg + '\r\n').encode('utf-8'))
         # this might be better with select(), but let's try this...
         while True:
             # @TODO handle exception on timeout
